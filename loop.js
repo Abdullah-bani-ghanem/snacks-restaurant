eeeaf63a50bfa5be688b66c589faae1b8428a30f
Your Name <abdullahbanighanem@gmile.com>
2024-12-29 20:25:30 +0300

    javascrept

---
+++ loop.js
@@ -0,0 +1,35 @@
+
+// for (let i = 0; i <= 5; i++) {
+//     alert(i); }
+
+
+
+
+
+// do {
+
+//     number = parseInt(prompt("Please enter a number between 0 and 100:"));
+  
+
+//     if (number < 0 || number > 100 || isNaN(number)) {
+//       alert("Invalid input! Please enter a number between 0 and 100.");
+//     }
+//   } while (number < 0 || number > 100 || isNaN(number));
+  
+
+//   alert("Thank you! You entered: " + number);
+
+
+
+
+
+
+let sum =0;
+let number =parseInt(prompt("Enter the integer numbr"));
+for(let i= 0 ; i <= number ; i++ ){
+sum+=i;
+}
+alert( "the sum = " + sum);
+
+
+
---
+++ snack.js
@@ -0,0 +1,77 @@
+// let name = window.prompt("Enter your name");
+
+// let gender = window.prompt("Enter his/her gender");
+
+// if (gender="male")
+// {
+//     // alert(`welcome Mr ${name}`);
+//     alert("welcome Mr " + name);
+
+// }else if(gender =female){
+//     alert(`welcom Ms ${name}`);
+
+// }else{
+//     alert("welcome person ")
+// }
+
+
+
+// let order =window.prompt("Enter your order shawarma, zinger, or burger");
+
+// alert("the order is being prepared")
+
+// console.log("the name is "+ name + " " +"the order is "+ order);
+
+
+
+// --------------------------------------------------------------------------
+
+
+
+
+
+let number = parseFloat(prompt("Enter the number"))
+
+switch(number){
+    case 1 :
+       console.log("ONE")
+       break;
+
+    case 2:
+        console.log("TOW")
+        break;
+     
+    case 3 :
+        console.log("THREE")
+        break;
+        
+    case 4 :
+         console.log("FOUR")
+        break;
+
+    case 5 :
+        console.log("FIVE")
+        break;
+
+    case 6 :
+        console.log("SIX")
+        break;
+
+
+    case 7 :
+        console.log("SEVEN")
+        break;
+
+    case 8 :
+        console.log("EIGHT")
+        break;
+
+    case 9 :
+        console.log("NINE")
+        break;
+
+        default :
+        console.log("the number is non")
+
+}
+
